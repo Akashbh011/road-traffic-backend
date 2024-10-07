7756593c53b58631b1a96fdc4f7b479fe34fbde8
ajinkya8010 <ajinkya8975@gmail.comcom>
2024-10-07 14:14:30 +0530

    controller-changes

--- routes/model.route.js
+++ routes/model.route.js
@@ -1,19 +1,12 @@
 import express from 'express';
 import getPrediction from '../controllers/model.controller.js';
-<<<<<<< HEAD
+
 import { upload } from '../middlewares/multer.middleware.js';
 
 const router = express.Router();
 
 
 router.post("/", upload.single('image'), getPrediction); 
-=======
-import { upload } from '../controllers/model.controller.js'; // Import the Cloudinary `upload` middleware
-
-const router = express.Router();
-
-router.post("/", upload.single('image'), getPrediction);  // Use the Cloudinary `upload`
->>>>>>> 818e49f54f73e96660a9a6a248487740359416ad
 
 export default router;
 
