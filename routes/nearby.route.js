6266f3b603a931338d8a4c4adab8b7ab6b8d2e57
ajinkya8010 <ajinkya8975@gmail.comcom>
2024-10-30 20:58:46 +0530

    file-renamed

--- routes/nearby.route.js
+++ routes/nearby.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { getNearbyplacedata } from '../controllers/getNearbyplacedata.controller.js';
+import { getNearbyplacedata } from '../controllers/nearby.controller.js';
 
 
 router.post('/',getNearbyplacedata); 
